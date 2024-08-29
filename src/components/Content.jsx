0172e044c643bbe61d250e9ef6041e7477468797
Yuki <98686305+Yuki-tyk@users.noreply.github.com>
2024-08-30 01:11:32 +0800

    remove Tailwind and better styling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,22 +4,22 @@ import Table from './Table'
 export default function Content(props) {
   const { hospitals, updatedTime, fetchAPIData } = props
   return (
-    <div className='flex-auto mx-auto mt-10 pt-4 relative max-w-full z-auto'>
-        <div className='px-2'>
-          <p className='text-base md:text-lg font-semibold text-red-500'>
-              Priority will be accorded to patients triaged as critical, emergency and urgent.
-          </p>
+    <div id="content">
+      <p id='clarity-statement'>
+          Priority will be accorded to patients triaged as critical, emergency and urgent.
+      </p>
 
-          <div className='mt-3 mx-auto place-items-center'>
-            As at {updatedTime}
-            <div className='float-right mr-1'>
-              <button onClick={fetchAPIData}>
-                <i className="fa-solid fa-rotate"></i>
-              </button>
-            </div>
+      <div id='table-container'>
+        <div id='updated-time-statement'>
+          As at {updatedTime}
+          <div id='reload-btn'>
+            <button onClick={fetchAPIData}>
+              <i className="fa-solid fa-rotate"></i>
+            </button>
           </div>
         </div>
         <Table hospitals={hospitals} updatedTime={updatedTime} fetchAPIData={fetchAPIData}/>
+      </div>
     </div>
   )
 }
